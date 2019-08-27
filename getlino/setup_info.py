ec063249bf92530887caf07699753197a24c2462
khchine5 <hamzakhchine@gmail.com>
2019-08-27 18:59:35 +0100

    Prepare to release the version 19.8.1

--- getlino/setup_info.py
+++ getlino/setup_info.py
@@ -1,6 +1,6 @@
 SETUP_INFO = dict(
     name='getlino',
-    version='19.8.0',
+    version='19.8.1',
     install_requires=['setuptools', 'click', 'virtualenv', 'cookiecutter', 'atelier'],
     test_suite='tests',
     description="Lino installer",
