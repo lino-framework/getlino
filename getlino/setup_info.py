383d637bcbee9614eb99022aa2e98df53d9a1836
Luc Saffre <luc.saffre@gmail.com>
2019-09-12 14:13:14 +0300

    http://luc.lino-framework.org/blog/2019/0912.html

--- getlino/setup_info.py
+++ getlino/setup_info.py
@@ -1,6 +1,6 @@
 SETUP_INFO = dict(
     name='getlino',
-    version='19.9.5',
+    version='19.9.6',
     install_requires=['setuptools', 'click', 'virtualenv', 'cookiecutter', 'atelier'],
     test_suite='tests',
     description="Lino installer",
