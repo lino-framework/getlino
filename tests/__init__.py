38f2ef153b40f226c1507762655a7e0d62051ee5
khchine5 <hamzakhchine@gmail.com>
2019-10-09 15:26:00 +0100

    Update tests on travis

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,3 +6,7 @@ from atelier.test import TestCase
 class PackagesTests(TestCase):
     def test_01(self):
         self.run_packages_test(SETUP_INFO['packages'])
+
+    def test_developer_mode(self):
+        self.run_subprocess(['getlino', 'configure'])
+        self.run_subprocess(['getlino', 'startsite', 'noi', 'first'])
--- tests/test_docker_prod.py
+++ tests/test_docker_prod.py
@@ -10,8 +10,3 @@ class DockerTests(TestCase):
     def test_prod_ubuntu(self):
         args = ['docker', 'run', 'prod_ubuntu', "ls -l"]
         self.run_subprocess(args)
-
-    def test_developer_mode(self):
-        self.run_subprocess(['getlino', 'configure'])
-        self.run_subprocess(['getlino', 'startsite', 'noi', 'first'])
-    
