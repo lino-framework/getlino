0600db7168b4120244828c12aeaec343fa4141ba
khchine5 <hamzakhchine@gmail.com>
2019-10-28 15:56:46 +0100

    Add --clone to configure command

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,5 +8,5 @@ class PackagesTests(TestCase):
         self.run_packages_test(SETUP_INFO['packages'])
 
     def test_developer_mode(self):
-        self.run_subprocess(['getlino','configure','--batch' ,'--db-engine', 'postgresql' ,'--db-port' ,'5432','--usergroup','travis'])
+        self.run_subprocess(['getlino','configure','--batch' ,'--db-engine', 'postgresql' ,'--db-port' ,'5432','--usergroup','travis','--clone'])
         self.run_subprocess(['getlino', 'startsite', '--batch', 'noi', 'mysite1', '--dev-repos', 'lino noi xl'])
