2d803c007ccacf102bacfd39dff716d97f515839
khchine5 <hamzakhchine@gmail.com>
2019-10-28 15:50:57 +0100

    Fix --dev-repos option

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,4 +9,4 @@ class PackagesTests(TestCase):
 
     def test_developer_mode(self):
         self.run_subprocess(['getlino','configure','--batch' ,'--db-engine', 'postgresql' ,'--db-port' ,'5432','--usergroup','travis'])
-        self.run_subprocess(['getlino', 'startsite', '--batch', 'noi', 'mysite1', '--dev-repos', '"lino noi xl"'])
+        self.run_subprocess(['getlino', 'startsite', '--batch', 'noi', 'mysite1', '--dev-repos', 'lino noi xl'])
