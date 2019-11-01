8c245bc508ee601df96834b320d5aa1f708bd38b
khchine5 <hamzakhchine@gmail.com>
2019-11-01 11:05:07 +0100

    Run prep.sh before testing

--- tests/test_docker_prod.py
+++ tests/test_docker_prod.py
@@ -4,9 +4,9 @@ import getlino
 
 class DockerTests(TestCase):
   def test_prod_debian(self):
-      self.run_subprocess('docker build -t prod_debian -f docker/prod/Dockerfile .'.split())
+      #self.run_subprocess('docker build -t prod_debian -f docker/prod/Dockerfile .'.split())
       self.run_subprocess(['docker', 'run', 'prod_debian', "ls -l"])
 
   def test_prod_ubuntu(self):
-      self.run_subprocess('docker build -t prod_debian -f docker/prod/Dockerfile_ubuntu .'.split())
+      #self.run_subprocess('docker build -t prod_debian -f docker/prod/Dockerfile_ubuntu .'.split())
       self.run_subprocess('docker run prod_ubuntu ls -l'.split())
