c665229dbe54b7544f2b388d640fc6bdd138d3ef
khchine5 <hamzakhchine@gmail.com>
2019-11-04 10:39:41 +0100

    Add more tests

--- tests/test_docker_prod.py
+++ tests/test_docker_prod.py
@@ -5,8 +5,16 @@ import getlino
 class DockerTests(TestCase):
   def test_prod_debian(self):
       #self.run_subprocess('docker build -t prod_debian -f docker/prod/Dockerfile .'.split())
-      self.run_subprocess(['docker', 'run', 'prod_debian', "ls -l"])
+      self.run_subprocess("docker run prod_debian /bin/bash -c 'cd /usr/local/lino/lino_local/mysite1 && ls -l'".split())
+      self.run_subprocess("docker run prod_debian /bin/bash -c 'cd /usr/local/lino/lino_local/mysite1 && ./pull.sh '".split())
+      self.run_subprocess("docker run prod_debian /bin/bash -c 'cd /usr/local/lino/lino_local/mysite1 && ./make_snapshot.sh '".split())
+      self.run_subprocess("docker run prod_debian /bin/bash -c '. /usr/local/lino/lino_local/mysite1/env/bin/activate && cd /usr/local/lino/lino_local/mysite1 && exec python manage.py runserver '".split())
+      self.run_subprocess("docker run prod_debian /bin/bash -c '. /usr/local/lino/lino_local/mysite1/env/bin/activate && cd /usr/local/lino/lino_local/mysite1 && exec python manage.py runserver '".split())
 
   def test_prod_ubuntu(self):
       #self.run_subprocess('docker build -t prod_debian -f docker/prod/Dockerfile_ubuntu .'.split())
       self.run_subprocess('docker run prod_ubuntu ls -l'.split())
+      self.run_subprocess("docker run prod_ubuntu /bin/bash -c 'cd /usr/local/lino/lino_local/mysite1 && ./pull.sh '".split())
+      self.run_subprocess("docker run prod_ubuntu /bin/bash -c 'cd /usr/local/lino/lino_local/mysite1 && ./make_snapshot.sh '".split())
+      self.run_subprocess("docker run prod_ubuntu /bin/bash -c 'cd /usr/local/lino/lino_local/mysite1 && ls -l'".split())
+      self.run_subprocess("docker run prod_ubuntu /bin/bash -c '. /usr/local/lino/lino_local/mysite1/env/bin/activate && cd /usr/local/lino/lino_local/mysite1 && exec python manage.py runserver '".split())
