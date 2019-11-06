6d1c0ef16dc86f4a42e8dc3a0a78eb47dc84388d
khchine5 <hamzakhchine@gmail.com>
2019-11-06 15:44:50 +0100

    Move getlion installation to the tests

--- tests/test_docker_prod.py
+++ tests/test_docker_prod.py
@@ -7,26 +7,27 @@ docker_ubuntu_command = 'docker run prod_ubuntu /bin/bash -c '.split()
 
 
 class DockerTests(TestCase):
+    def run_commands_for(self, docker_command):
+        if docker_command:
+            self.run_subprocess(
+                docker_command + ['sudo -H getlino configure --batch --db-engine postgresql'])
+            self.run_subprocess(
+                docker_command + ['sudo -H getlino startsite --batch noi mysite1 --dev-repos "lino noi xl"'])
+            self.run_subprocess(
+                docker_command + ['cd /usr/local/lino/lino_local/mysite1 && ls -l'])
+            self.run_subprocess(
+                docker_command + ['. /usr/local/lino/lino_local/mysite1/env/bin/activate && pull.sh '])
+            self.run_subprocess(
+                docker_command + ['cd /usr/local/lino/lino_local/mysite1 && ./make_snapshot.sh '])
+            self.run_subprocess(
+                docker_command + ['. /usr/local/lino/lino_local/mysite1/env/bin/activate && cd /usr/local/lino/lino_local/mysite1 && exec python manage.py runserver '])
+            self.run_subprocess(
+                docker_command + ['. /usr/local/lino/lino_local/mysite1/env/bin/activate && cd /usr/local/lino/lino_local/mysite1 && exec python manage.py runserver'])
+        else:
+            print("You need to set the docker command")
+
     def test_prod_debian(self):
-        self.run_subprocess(
-            docker_debian_command + ['cd /usr/local/lino/lino_local/mysite1 && ls -l'])
-        self.run_subprocess(
-            docker_debian_command + ['. /usr/local/lino/lino_local/mysite1/env/bin/activate && pull.sh '])
-        self.run_subprocess(
-            docker_debian_command + ['cd /usr/local/lino/lino_local/mysite1 && ./make_snapshot.sh '])
-        self.run_subprocess(
-            docker_debian_command + ['. /usr/local/lino/lino_local/mysite1/env/bin/activate && cd /usr/local/lino/lino_local/mysite1 && exec python manage.py runserver '])
-        self.run_subprocess(
-            docker_debian_command + ['. /usr/local/lino/lino_local/mysite1/env/bin/activate && cd /usr/local/lino/lino_local/mysite1 && exec python manage.py runserver'])
+        self.run_commands_for(docker_debian_command)
 
     def test_prod_ubuntu(self):
-        self.run_subprocess(
-            docker_ubuntu_command + ['cd /usr/local/lino/lino_local/mysite1 && ls -l'])
-        self.run_subprocess(
-            docker_ubuntu_command + ['. /usr/local/lino/lino_local/mysite1/env/bin/activate && pull.sh '])
-        self.run_subprocess(
-            docker_ubuntu_command + ['cd /usr/local/lino/lino_local/mysite1 && ./make_snapshot.sh '])
-        self.run_subprocess(
-            docker_ubuntu_command + ['. /usr/local/lino/lino_local/mysite1/env/bin/activate && cd /usr/local/lino/lino_local/mysite1 && exec python manage.py runserver '])
-        self.run_subprocess(
-            docker_ubuntu_command + ['. /usr/local/lino/lino_local/mysite1/env/bin/activate && cd /usr/local/lino/lino_local/mysite1 && exec python manage.py runserver'])
+        self.run_commands_for(docker_ubuntu_command)
