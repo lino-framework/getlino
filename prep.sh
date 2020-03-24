fa7e4e112443ea71b4067d8b9f729a92fcfb2a24
Hamza Khchine <hamzakhchine@gmail.com>
2020-03-24 10:41:46 +0100

    Add again test about ubuntu

--- prep.sh
+++ prep.sh
@@ -1,3 +1,3 @@
 set -e  # exit on error
 docker build --no-cache -t getlino_debian  -f docker/prod/Dockerfile .
-#docker build --no-cache -t getlino_ubuntu  -f docker/prod/Dockerfile_ubuntu .
+docker build --no-cache -t getlino_ubuntu  -f docker/prod/Dockerfile_ubuntu .
