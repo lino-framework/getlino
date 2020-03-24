50dc1aeb75f36690fbcc0cba07dd139217fc6905
Hamza Khchine <hamzakhchine@gmail.com>
2020-03-24 09:47:23 +0100

    Use buster-backports to install monit for debian

--- getlino/configure.py
+++ getlino/configure.py
@@ -273,6 +273,10 @@ def configure(ctx, batch,
         i.apt_install("graphviz sqlite3")
 
     if DEFAULTSECTION.getboolean('monit'):
+        if platform.dist()[0].lower() == "debian":
+            i.runcmd("""printf "%s\n" "deb http://ftp.de.debian.org/debian buster-backports main" | \
+                        sudo tee /etc/apt/sources.list.d/buster-backports.list""")
+            i.runcmd("apt-get update -y")
         i.apt_install("monit")
 
     if DEFAULTSECTION.getboolean('redis'):
