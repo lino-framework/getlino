e1df7820ef2f0bafbeabd139f46996dc5e47c3c2
Hamza Khchine <hamzakhchine@gmail.com>
2020-04-02 15:27:45 +0100

    Prepare to release 20.4.01

--- getlino/setup_info.py
+++ getlino/setup_info.py
@@ -1,6 +1,6 @@
 SETUP_INFO = dict(
     name='getlino',
-    version='20.1.2',
+    version='20.4.01',
     install_requires=['click', 'virtualenv', 'jinja2'],
     # tests_require=['docker', 'atelier'],
     # test_suite='tests',
