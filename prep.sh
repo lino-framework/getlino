c31ad349727f21f062308793b46fcb8399562491
Luc Saffre <luc.saffre@gmail.com>
2020-07-14 12:10:09 +0300

    http://luc.lino-framework.org/blog/2020/0714.html

--- prep.sh
+++ prep.sh
@@ -1,3 +1,4 @@
 set -e  # exit on error
-docker build --no-cache -t getlino_debian  -f docker/prod/Dockerfile .
-docker build --no-cache -t getlino_ubuntu  -f docker/prod/Dockerfile_ubuntu .
+# docker image prune -f
+docker build --no-cache -t getlino_debian -f docker/prod/Dockerfile .
+docker build --no-cache -t getlino_ubuntu -f docker/prod/Dockerfile_ubuntu .
