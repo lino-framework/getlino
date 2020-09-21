1a2e065b0966e44622f65b7a46770f4111311adb
Luc Saffre <luc.saffre@gmail.com>
2020-09-21 11:36:47 +0300

    publish to pypi

--- getlino/setup_info.py
+++ getlino/setup_info.py
@@ -1,6 +1,6 @@
 SETUP_INFO = dict(
     name='getlino',
-    version='20.7.5',
+    version='20.9.0',
     install_requires=['click', 'virtualenv', 'jinja2', 'distro'],
     # tests_require=['docker', 'atelier'],
     # test_suite='tests',
